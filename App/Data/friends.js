59cb7b0611c5715121a18b0a00453efef4eaa506
Daniel Osborne <deeos34@gmail.com>
2018-04-29 11:18:57 -0400

    Almost there

---
+++ App/Data/friends.js
@@ -0,0 +1,20 @@
+var friendsArray = [
+	{
+		name: "Dan1",
+		photoURL: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAdcAAAAJDE4OGM4YTA1LWE4NzYtNGM3Yi1iZTRjLTBhOGE4NDhhZWE5MA.jpg",
+		scores:[
+		    5,
+		    1,
+		    4,
+		    4,
+		    5,
+		    1,
+		    2,
+		    5,
+		    4,
+		    1
+		]
+	},
+];
+
+module.exports = friendsArray;
